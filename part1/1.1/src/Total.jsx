9cc914854dd050df6f107ef797af1273bcea7561
emanuelmoya94 <emanuelmoya11@gmail.com>
2026-04-03 11:52:27 -0300

    ejercicio 1.3- 1.5

--- part1/1.1/src/Total.jsx
+++ part1/1.1/src/Total.jsx
@@ -1,7 +1,13 @@
-const Total = (props)=>{
-    return (
-        <p> Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    )
-}
+const Total = (props) => {
+  return (
+    <p>
+      {" "}
+      Number of exercises{" "}
+      {props.course.parts[0].exercises +
+        props.course.parts[1].exercises +
+        props.course.parts[2].exercises}
+    </p>
+  );
+};
 
 export default Total;
